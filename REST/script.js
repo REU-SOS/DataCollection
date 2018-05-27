8cb561d533d4224da54aa281f4e2cb8f7a2798c5
theisencr <theisen.cr@gmail.com>
2018-05-27 15:55:03 -0400

    Adding a bit more of a skeleton to the base script file

--- REST/script.js
+++ REST/script.js
@@ -18,6 +18,7 @@ getYourRepos(userId);
 function getYourRepos(userName)
 {
 
+	//commented code is a skeleton for sending information to the API
 	var options = {
 		url: urlRoot + '/users/' + userName + "/repos",
 		method: 'GET',
@@ -26,13 +27,23 @@ function getYourRepos(userName)
 			"content-type": "application/json",
 			"Authorization": token
 		}
+		/*
+		json: {
+			"var1": "1",
+			"var2": true
+		}
+		*/
 	};
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	request(options, function (error, response, body)
 	{
+		// by default, print out each of the return fields so you can construct a parser.
 		console.log('error:', error);
 		console.log('statusCode:', response && response.statusCode);
+		console.log('body:', body);
+
+		// parse the returned data (in "body")
 		var obj = JSON.parse(body);
 		console.log( obj );
 		for( var i = 0; i < obj.length; i++ )
